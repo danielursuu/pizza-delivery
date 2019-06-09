0165dff9186a7ae801de3579b93b606c7b42106a
Loredana <loredanapurel@yahoo.com>
2019-06-09 17:41:18 +0300

    Removed unused directives

--- Migrations/PizzaContextModelSnapshot.cs
+++ Migrations/PizzaContextModelSnapshot.cs
@@ -2,7 +2,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PizzaDeliveryAPI.Data;
 
 namespace PizzaDeliveryAPI.Migrations
